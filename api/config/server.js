f8d84f2fb1e008fa7a031ad7f45c4c115d8522f5
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-02-27 16:13:47 +0000

    startup repo

--- api/config/server.js
+++ api/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8002),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9feea47cab3957fe9fa164e4eb6238ad'),
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07b032c7-92d9-43a9-9629-9ca4e56b4915'
+};
