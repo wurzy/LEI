1ad687b56d8c5ff8a890ac84cb4f9743564ff913
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-02-27 16:08:17 +0000

    Initial commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9feea47cab3957fe9fa164e4eb6238ad'),
+    },
+  },
+});
