e0e7f1b2447d29c4a9f9c362282fedd545ec00bf
fliper6 <filipa.alvesantos@outlook.com>
2021-03-05 05:48:10 +0000

    new api (strapi adaptado a mongoose)

--- api/config/server.js
+++ api/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8002),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9feea47cab3957fe9fa164e4eb6238ad'),
+      secret: env('ADMIN_JWT_SECRET', '67b807abb3fe450f9368d41488361aa8'),
     },
   },
 });
