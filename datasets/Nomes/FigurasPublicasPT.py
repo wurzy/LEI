e5ab1b9f35268db4391dc011a2d20140b4b75137
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-03-11 21:41:57 +0000

    dataset fiesta

---
+++ datasets/Nomes/FigurasPublicasPT.py
@@ -0,0 +1,56 @@
+Ana Guiomar
+Andreia Rodrigues
+António Raminhos
+António Zambujoúsico
+Áureaúsico
+Bruno Nogueira
+Carminhoúsico
+Carolina Patrocínio
+Catarina Furtado
+César Mourão
+Cláudia Vieira
+Cristiano Ronaldo
+Cristina Ferreira
+Dânia Neto
+Daniela Ruah
+David Carreiraúsico
+Diana Chaves
+Fátima Lopes
+Fernanda Serrano
+Fernando Mendes
+Filomena Cautela
+Gisela Joãoúsico
+Herman José
+Jessica Athayde
+Joana Duarte
+João Manzarra
+José Fidalgo
+José Mourinho
+Júlia Pinheiro
+Lourenço Ortigão
+Lúcia Monizúsico
+Manuel Luís Goucha
+Manuel Marques
+Marizaúsico
+Mickael Carreiraúsico
+Nelson Évora
+Nilton
+Nuno Lopes
+Nuno Markl
+Patrícia Mamona
+Pedro Fernandes
+Pedro Teixeira
+Raquel Strada
+Ricardo Araújo Pereira:
+Ricardo Pereira
+Rita Blanco
+Rita Pereira
+Rui Unas
+Salvador Martinha
+Salvador Sobralúsico
+Sara Matos
+Sara Sampaio
+Sílvia Alberto
+Sónia Araújo
+Telma Monteiro
+Vasco Palmeirim
--- datasets/tratarNomes.js
+++ datasets/tratarNomes.js
@@ -1,9 +1,24 @@
-var fs = require('fs')
-var txt = fs.readFileSync('./listaNomesVal2.txt').toString().split(/\r?\n/)
+const fs = require('fs')
+const txt1 = fs.readFileSync('./Person/listaNomesVal2.txt').toString().split(/\r?\n/)
+const txt2 = fs.readFileSync('./Person/FigurasPublicasPT.py').toString().split(/\r?\n/)
+const txt3 = fs.readFileSync('./Person/Futebolista.py').toString().split(/\r?\n/)
+const txt4 = fs.readFileSync('./Person/Pessoas - Vários.py').toString().split(/\r?\n/)
+const txt5 = fs.readFileSync('./Person/Político.py').toString().split(/\r?\n/)
+const txt6 = fs.readFileSync('./Person/Top100Portugueses.py').toString().split(/\r?\n/)
+const txt7 = fs.readFileSync('./Person/WorldTop100PublicPeople.py').toString().split(/\r?\n/)
+const txt8 = fs.readFileSync('./Person/ator.txt').toString().split(/\r?\n/)
+const txt9 = fs.readFileSync('./Person/empresario_pt.txt').toString().split(/\r?\n/)
+const txt10 = fs.readFileSync('./Person/escritor_estrangeiro.txt').toString().split(/\r?\n/)
+
+var mega = txt1.concat(txt2,txt3,txt4,txt5,txt6,txt7,txt8,txt9,txt10)
+
 var finalJSON = []
-txt.forEach(nomeCompleto => {
+mega.forEach(nomeCompleto => {
     let [nome, ...sobrenome] = nomeCompleto.split(' ')
     sobrenome = sobrenome.join(' ')
+
+    if(nome == "" || sobrenome == "") return
+
     finalJSON.push({nome: nome, sobrenome: sobrenome})
 })
 
