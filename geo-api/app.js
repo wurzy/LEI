d5376a915b5f885f5d56b1958fbc320da74f28d6
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-03-11 16:19:34 +0000

    update geo-api

--- geo-api/app.js
+++ geo-api/app.js
@@ -26,17 +26,17 @@ app.use('/distritos', distritosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
-  
+
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // send error to user
-    res.status(err.status || 500);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error 
+  res.status(err.status || 500).jsonp(err);
 });
 
 module.exports = app;
