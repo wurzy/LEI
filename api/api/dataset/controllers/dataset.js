0fdb2206ec175ad4c85088a764c5bfb820242d30
Jcc20 <48126929+Jcc20@users.noreply.github.com>
2021-03-23 15:09:12 +0000

    Criação das pastas no strapi pela api feito

--- api/api/dataset/controllers/dataset.js
+++ api/api/dataset/controllers/dataset.js
@@ -1,5 +1,5 @@
 'use strict';
-
+  
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
