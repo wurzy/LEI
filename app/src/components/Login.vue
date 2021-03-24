4eb69c91d2f55ba3d4cae0eafc076540cfdf52b4
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-03-24 01:49:07 +0000

    autenticação brr

---
+++ app/src/components/Login.vue
@@ -0,0 +1,167 @@
+<template>
+<div>
+    <div id="login_modal" class="modal fade">
+      <div class="modal-dialog modal-login">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <div class="avatar">
+                      <img src="https://www.tutorialrepublic.com/examples/images/avatar.png" alt="Avatar">
+                  </div>
+                  <h4 class="modal-title">Bem-vindo!</h4>
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              </div>
+              <div class="modal-body">
+                  <form @submit.prevent="handleLogin">
+                      <div class="form-group">
+                          <input v-model="email" type="text" class="form-control" name="email" placeholder="E-mail"
+                              required="required">
+                      </div>
+                      <div class="form-group">
+                          <input v-model="password" type="password" class="form-control" name="password" placeholder="Palavra-passe"
+                              required="required">
+                      </div>
+                      <div class="form-group">
+                          <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Login</button>
+                      </div>
+                  </form>
+              </div>
+              <div class="modal-footer">
+                  <a href="#">Forgot Password?</a>
+              </div>
+          </div>
+      </div>
+  </div> 
+</div>
+</template>
+
+<script>
+import $ from 'jquery';
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:3000/";
+
+export default {
+    name: "Login",
+    data() {
+        return {
+          email: "",
+          password: "",
+        }
+    },
+    methods: {
+        async handleLogin(){
+            const res = await axios.post('utilizadores/login', {
+              email: this.email,
+              password: this.password
+            })
+            $("#login_modal").modal("hide");
+            if(res.status==201){
+                console.log("ok")
+                $("#login_success_modal").modal("show");
+                $("#login_success_modal").css("z-index", "1500");
+                localStorage.setItem('token',res.data.token)
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.modal-login {
+  color: #636363;
+  width: 350px;
+  margin: 80px auto 0;
+}
+.modal-login .modal-content {
+  padding: 20px;
+  border-radius: 5px;
+  border: none;
+}
+.modal-login .modal-header {
+  border-bottom: none;
+  position: relative;
+  justify-content: center;
+}
+.modal-login h4 {
+  text-align: center;
+  font-size: 26px;
+  margin: 30px 0 -15px;
+}
+.modal-login .form-control:focus {
+  border-color: #007bff;
+}
+.modal-login .form-control,
+.modal-login .btn {
+  min-height: 40px;
+  border-radius: 3px;
+}
+.modal-login .close {
+  position: absolute;
+  top: -5px;
+  right: -5px;
+}
+.modal-login .modal-footer {
+  background: #ecf0f1;
+  border-color: #dee4e7;
+  text-align: center;
+  justify-content: center;
+  margin: 0 -20px -20px;
+  border-radius: 5px;
+  font-size: 13px;
+}
+.modal-login .modal-footer a {
+  color: #999;
+}
+.modal-login .avatar {
+  position: absolute;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  top: -70px;
+  width: 95px;
+  height: 95px;
+  border-radius: 50%;
+  z-index: 1055;
+  background: #007bff;
+  padding: 15px;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+}
+.modal-login .avatar img {
+  width: 100%;
+}
+.modal-login .btn {
+  color: #fff;
+  border-radius: 4px;
+  background: #007bff;
+  text-decoration: none;
+  transition: all 0.4s;
+  line-height: normal;
+  border: none;
+}
+.modal-login .btn:hover,
+.modal-login .btn:focus {
+  background: #007bff;
+  outline: none;
+}
+.modal-dialog{
+  width:500px;
+}
+.modal {
+  text-align: center;
+  padding: 0!important;
+}
+
+.modal:before {
+  content: '';
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+  margin-right: -18px;
+}
+
+.modal-dialog {
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
+  margin-bottom: 200px;
+}
+</style>
--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <template>
- <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
-  <div class="container">
+<div>
+  <Login />
+  <Register />
+  <Success msg="Registo efetuado com sucesso!" id="register_success_modal"/>
+  <Success msg="Login efetuado com sucesso!" id="login_success_modal"/>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+   <div class="container">
     <a class="navbar-brand" href="/">Gerador de Datasets</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -31,7 +37,7 @@
             <a class="nav-link" href="documentation">Documentação</a>
           </li>
         </div>
-                <div v-if="isAbout">
+        <div v-if="isAbout">
           <li class="nav-item active">
             <a class="nav-link" href="about">Sobre
               <span class="sr-only">(current)</span>
@@ -43,28 +49,91 @@
             <a class="nav-link" href="about">Sobre</a>
           </li>
         </div>
+        <div>
+          <li v-if="isLoggedIn" class="nav-item">
+            <a href="#" class="nav-link" @click="logout">Logout</a>
+          </li>
+        </div>
+        <div>
+          <li v-if="!isLoggedIn" class="nav-item">
+            <a href="#" class="nav-link" @click="login">Login</a>
+          </li>
+        </div>
+        <div>
+          <li v-if="!isLoggedIn" class="nav-item">
+            <a href="#" class="nav-link" @click="registar">Registar</a>
+          </li>
+        </div>
       </ul>
     </div>
   </div>
-</nav>
+ </nav>
+</div>
 </template>
 
 <script>
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Success from './Success.vue';
+import Register from './Register.vue';
+import Login from './Login.vue';
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery';
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:3000/";
 
 export default {
-  name: 'Navbar',
-  computed:{
+  name: "Navbar",
+  components: {
+    Success,
+    Register,
+    Login
+  },
+  data(){
+    return {
+      utilizador: null
+    }
+  },
+  computed: {
     isHome() {
-      return this.$route.name == "Home"
+      return this.$route.name == "Home";
     },
     isAbout() {
-      return this.$route.name == "About"
+      return this.$route.name == "About";
     },
-    isDocumentation(){
-      return this.$route.name == "Documentation"
+    isDocumentation() {
+      return this.$route.name == "Documentation";
+    },
+    isLoggedIn(){
+      return localStorage.getItem('token')!=null
+    }
+  },
+  async created() {
+    const token = localStorage.getItem('token')
+    console.log("render")
+    if(token){
+      const res = await axios.get('utilizadores/' + localStorage.getItem('token'))
+      this.utilizador = res.data
+      console.log(res.data)
+    }
+    else {
+      console.log("not logged in")
+    }
+  },
+  methods: {
+    login(){
+      $("#login_modal").modal("show");
+      $("#login_modal").css("z-index", "1500");
+    },
+    registar(){
+      $("#registar_modal").modal("show");
+      $("#registar_modal").css("z-index", "1500");
+    },
+    logout(){
+      localStorage.removeItem('token')
+      this.$router.go(this.$router.currentRoute)
     }
   }
-}
+};
 </script>
