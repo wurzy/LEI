37000c7c5c129a76f4ad2698d996d343f859acb5
fliper6 <filipa.alvesantos@outlook.com>
2021-04-21 03:07:09 +0100

    Pagina sobre atualizada

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,126 +1,92 @@
 <template>
-   <div>
-      <!-- Hero -->
-    <section id="hero2">
-        <div class="container">
-            <div class="heading-wrapper">
-                <h1>Gerador de Datasets</h1>
-                <p> A FIP nasceu da necessidade de resolver um problema muito comum nos estudantes universitários portugueses: a dificuldade em levar a cabo os seus próprios projetos. Também nós, estudantes universitários, estamos a par das limitações que impedem os jovens de desenvolverem as suas ideias e pô-las em prática. </p>
-                <p> A nossa missão é conectar os estudantes universitários portugueses e ensiná-los a transformar as suas ideias em ações. São estes estudantes que, independentemente da sua área de trabalho ou curso, sentem maior necessidade em criar algo seu, fora do contexto curricular. Assim, seja para ganhar experiência, realizar networking, desenvolver o currículo ou puramente por diversão, a FIP é o seu destino. </p>
-                <p> Sabemos que os jovens portugueses escondem um grande potencial que não pode deixar de ser explorado. A ambição e originalidade presente na nossa comunidade são a chave para um futuro promissor. Por isso, somos a Face da Inovação Portuguesa. <b>A FIP é a Tua.</b></p>
-            </div>
-        </div>
-    </section>
-    <!-- End Hero -->
+   <div class="container">
+    <br/>
+    <h2>Sobre</h2>
+    <hr/>
+    <div class="intro">
+        <h4>Introdução</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+         dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+         veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
+         consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci 
+         velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem 
+         ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil 
+         molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+    </div>
 
-    <!-- Team -->
-    <section id="team">
-        <div class="container">
-            <div class="heading-wrapper">
-                <h2><mark class="mark">A nossa equipa</mark></h2>
-            </div>
-            <div class="row topmargin-lg">
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_diogo.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Diogo Cunha</h5>
-                            <b>Gestão do Projeto</b>
-                            <p> </p>
-                            <p>Dinâmico e apaixonado por gerir pessoas, ideias e projetos. Orientando-se por objetivos bem delineados faz tudo por concretizar uma visão ambiciosa. Adora jogos RPG e quando não está a falar de coisas sérias é uma fonte de aleatoriedade engraçada. Frequenta o 4° ano do Mestrado Integrado em Engenharia e Gestão Industrial na FEUP.</p>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/diogocostacunhaa/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/diogocostacunha/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_vasco.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Vasco Carvalho</h5>
-                            <b>Operações</b>
-                            <p> </p>
-                            <p>Viciado em jogos de estratégia, desafios e em decifrar enigmas. É persistente e não descansa enquanto não encontrar a solução. Não hesita em ajudar os colegas e gosta de discutir temas atuais. Frequenta o 4° ano do Mestrado Integrado em Engenharia e Gestão Industrial na FEUP.</p>
-                            <br/>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/vasco.carvalho.44/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/vasco-carvalho-950aa41b5/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_filipa.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Filipa Santos</h5>
-                            <b>Marketing e Design</b>
-                            <p> </p>
-                            <p>Perita em organização, não descansa até tudo estar perfeito. Os seus vícios atuais são Valorant e Animal Crossing. Outros dos seus interesses incluem edição de vídeo, cortar o cabelo às 3 da manhã e festivais de verão. Frequenta o 4° ano do Mestrado Integrado em Engenharia Informática na UM.</p>
-                            <br/>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/fliper.santos/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/filipa-santos-00111b1b5/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row topmargin-lg">
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_hugo.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Hugo Cardoso</h5>
-                            <b>Programação</b>
-                            <p> </p>
-                            <p>Ambicioso, metódico e apaixonado pela programação e informática em geral. É também rendido à prática de desportos em equipa, em momentos proativos, e fã de música, séries e videojogos, nos momentos mais relaxados. Frequenta o 4° ano do Mestrado Integrado em Engenharia Informática na UM.</p>                           
-                            <br/>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/hugocardoso30/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/hugo-cardoso-b868a474/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_ivo.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Ivo Baixo</h5>
-                            <b>Programação</b>
-                            <p> </p>
-                            <p>Curioso por todas as áreas do saber, procura constantemente novos desafios que o levem a crescer. Sempre pronto para perder aquele jogo casual com amigos e niilista nos seus tempos livres. Frequenta o 4° ano do Mestrado Integrado em Engenharia Informática na UM.</p>
-                            <br/> <br/>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/baixo.so.de.nome/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/ivo-baixo-8b0bb9149/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="team-post">
-                        <a><img src="assets/images/team_balao.png" class="img-fluid" alt=""></a>
-                        <div class="team-post-info">
-                            <h5>Gonçalo Balão</h5>
-                            <b>Programação</b>
-                            <p> </p>
-                            <p>Sempre interessado em aprender coisas novas e à procura de novas experiências. Quer seja a superar-se nos videojogos, a praticar desporto ou a tentar tocar as suas músicas favoritas, enfrenta tudo com grande empenho. Frequenta o 4° ano do Mestrado Integrado em Engenharia Aeroespacial no IST.</p>
-                            <br/>
-                            <div class="col-md-12" id="social">
-                              <a href="https://www.instagram.com/el._.balloon/" target="_blank"><i class="icon ion-logo-instagram"></i></a>
-                              <a href="https://www.linkedin.com/in/gon%C3%A7alo-silva-83250a1b2/" target="_blank"><i class="icon ion-logo-linkedin"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <hr/>
+    <h4>Equipa</h4>
+    
+    <div class="team-post row topmargin-lg">
+        <div class="col-md-3" style="justify-content: center; align-items: center; display: flex;">
+            <a><img src="../assets/user-default.png" class="img-fluid" alt=""></a>
+        </div>
+        <div class="col-md-9 team-post-info">
+            <h5>Filipa Santos</h5>
+            <b>Programador</b>
+            <p> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>                           
+            <div id="social" class="rightS">
+                <a href="https://github.com/fliper6" target="_blank" class="fa fa-github" style="margin-right:10px"></a>
+                <a href="https://www.linkedin.com/in/filipa-santos-00111b1b5/" target="_blank" class="fa fa-linkedin"></a>
             </div>
         </div>
-    </section>
-    <!-- End Team -->
+    </div>
+
+    <div class="team-post row topmargin-lg">
+        <div class="col-md-9 team-post-info" style="text-align: right">
+            <h5>Hugo Cardoso</h5>
+            <b>Programador</b>
+            <p> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>                           
+            <div id="social" class="leftS">
+                <a href="https://github.com/Abjiri" target="_blank" class="fa fa-github" style="margin-right:10px"></a>
+                <a href="https://www.linkedin.com/in/hugo-cardoso-b868a474/" target="_blank" class="fa fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="col-md-3" style="justify-content: center;  align-items: center; display: flex;">
+            <a><img src="../assets/user-default.png" class="img-fluid" alt=""></a>
+        </div>
+    </div>
+
+    <div class="team-post row topmargin-lg">
+        <div class="col-md-3" style="justify-content: center; align-items: center; display: flex;">
+            <a><img src="../assets/user-default.png" class="img-fluid" alt=""></a>
+        </div>
+        <div class="col-md-9 team-post-info">
+            <h5>João Cunha</h5>
+            <b>Programador</b>
+            <p> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>                           
+            <div id="social" class="rightS">
+                <a href="https://github.com/Jcc20" target="_blank" class="fa fa-github" style="margin-right:10px"></a>
+                <a href="" target="_blank" class="fa fa-linkedin"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="team-post row topmargin-lg">
+        <div class="col-md-9 team-post-info" style="text-align: right">
+            <h5>Válter Carvalho</h5>
+            <b>Programador</b>
+            <p> </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>                           
+            <div id="social" class="leftS">
+                <a href="https://github.com/wurzy" target="_blank" class="fa fa-github" style="margin-right:10px"></a>
+                <a href="https://www.linkedin.com/in/valterfpcarvalho/" target="_blank" class="fa fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="col-md-3" style="justify-content: center;  align-items: center; display: flex;">
+            <a><img src="../assets/user-default.png" class="img-fluid" alt=""></a>
+        </div>
+    </div>
+
    </div>
 </template>
 
@@ -132,19 +98,6 @@
 
 html {
     scroll-behavior: smooth;
-}
-
-body {
-    background-color: #ffffff;
-    color: #5b617c;
-    font-family: 'Roboto', sans-serif;
-    position: relative;
-}
-
-h1, h2, h3, h4, h5, h6 {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 700;
-    color: #282c47;
 }
 
 a:hover {
@@ -165,121 +118,12 @@ section {
     margin-top: 60px;
 }
 
-.primary-btn {
-    background-color: #669999;
-    color: #ffffff;
-    display: inline-block;
-    font-size: 16px;
-    margin: 5px;
-    padding: 14px 30px;
-    border-radius: 4px;
-}
-
-.secondary-btn {
-    background-color: #d9d9d9;
-    color: #5E5E5E;
-    display: inline-block;
-    font-size: 16px;
-    margin: 5px;
-    padding: 14px 30px;
-    border-radius: 4px;
-}
-
-.primary-btn:hover, .secondary-btn:hover {
-    text-decoration: none;
-}
-
-.primary-btn:hover {
-    color: #ffffff;
-}
-
-.template-button {
-    position: fixed;
-    padding: 10px 30px;
-    border-radius: 5px;
-    background-color: #38C2B4;
-    color: #ffffff;
-    z-index: 9999;
-    bottom: 15px;
-    left: 15px;
-    display: inline-block;
-}
-
-.template-button a {
-    color: #ffffff;
-}
-
-.template-button i {
-    vertical-align: middle;
-    margin-left: 10px;
-}
-
-.template-button:hover {
-    z-index: 9999;
-}
-
-.template-button p {
-    margin-bottom: 0px;
-}
-
-.heading-wrapper {
-    margin: 0 auto;
-    text-align: center;
-}
-
-.heading-wrapper-max {
-    max-width: 800px;
-    margin: 0 auto;
-    text-align: center;
-}
-/*/////////////////////////////////////////
-Hero
-////////////////////////////////////////*/
-
-#hero {
-    text-align: center;
-    font-size: 18px;
-    margin-top: 60px;
-    background-repeat: no-repeat;
-    background-position: center;
-}
-
-#hero h1 {
-    font-size: 38px;
-    margin-bottom: 30px;
-}
-
-#hero p {
-    margin-bottom: 18px;
-}
-
-#hero2 {
-    text-align: center;
-    font-size: 18px;
-    margin-top: 60px;
-    background-repeat: no-repeat;
-    background-position: center;
-}
-
-#hero2 h1 {
-    font-size: 38px;
-    margin-bottom: 30px;
-}
-
-#hero2 p {
-    font-size: 17px;
-    margin-bottom: 18px;
-}
 .mark{
     background-color: #B3DADA;   
 }
 
-/*/////////////////////////////////////////
-Team
-////////////////////////////////////////*/
-
-#team {
-    background-color: #f4f4f6;
+.intro {
+    margin-bottom: 40px !important;
 }
 
 .team-post {
@@ -287,7 +131,6 @@ Team
     box-shadow: #d7d8e2 0px 2px 4px 0px;
     border-bottom-left-radius: 4px;
     border-bottom-right-radius: 4px;
-    min-height: 790px;
     margin: 15px 0; 
     transition: all .2s ease-in-out;
     filter: grayscale(100%);
@@ -298,17 +141,30 @@ Team
     filter: grayscale(0%);
 }
 
+.
 .team-post img {
+    max-width: 200px;
     border-top-left-radius: 4px;
     border-top-right-radius: 4px;
 }
 
 .team-post-info {
     padding: 24px;
-    text-align: center;
 }
 
 #social {
+    font-size: 1.2em;
     filter: grayscale(100%);
 }
+
+.rightS {
+    float: left;
+    margin-right: 15px;
+}
+
+.leftS {
+    float: right;
+    margin-left: 15px;
+}
+
 </style>
